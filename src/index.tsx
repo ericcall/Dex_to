d32cb1d7e42e74d87e9edac9875f94debb93ec95
engineer <engineerstar1426@gmail.com>
2026-01-13 09:16:58 -0500

    first

---
+++ src/index.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "regenerator-runtime/runtime";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./App/App";
+import reportWebVitals from "./reportWebVitals";
+
+declare global {
+  interface Window {
+    WidgetApp?: {
+      mount: (elementId: string, option: { userId: string}) => void;
+      unmount?: () => void;
+    }
+  }
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.info))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
